50f51531113a976a70361afcf5fafd3cf70ab907
Marcelo Vogt <marcelinho.vogt@gmail.com>
2017-06-12 16:39:44 -0300

    Inteligência Artificial Pronta

--- src/com/space/cows/Main.java
+++ src/com/space/cows/Main.java
@@ -1,24 +1,35 @@
 package com.space.cows;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Lhaion on 07/06/2017.
  */
 public class Main {
 
-	private static String[] prevs = new String[2];
+
+	private static final Random random = new Random();
 
 	public static void main(String[] str) {
 
-		prevs[0] = "";
-		prevs[1] = "";
+		int[][] prevs = new int[2][2];
+
+		prevs[0] = new int[]{-1, -1};
+		prevs[1] = new int[]{-1, -1};
 
 		Scanner scanner = new Scanner(System.in);
 
 		int[][] board = new int[7][7];
 
 		boolean yourTurn = true;
+
+		System.out.println("Qual vai ser a ordem de jogo? (2 dígitos)\n[p][j] jogador\n[c][a] computador");
+
+		String gamemode = scanner.next().toUpperCase().substring(0, 2);
+
+		System.out.println("What difficulty would you want? (0-2)");
+
+		int difficulty = scanner.nextInt() % 10;
 
 		do {
 
@@ -30,11 +41,42 @@ public class Main {
 
 			printBoard(board);
 
-			println("Onde você vai jogar?");
+			int turn = yourTurn ? 0 : 1;
 
-			String pos = scanner.nextLine();
+			int[] move = new int[2];
 
-			if (play(pos, board, yourTurn)) {
+			Character gameTurn = gamemode.charAt(turn);
+
+			if (gameTurn.equals('C') || gameTurn.equals('A')) {
+
+//				sleep(random.nextInt(2000) + 500);
+
+				move = aiTurn(board, yourTurn, difficulty, prevs);
+
+			} else {
+
+				println("Onde você vai jogar?");
+
+				String pos = scanner.next().toUpperCase();
+
+				char c1 = pos.charAt(0);
+				char c2 = pos.charAt(1);
+
+				if (Character.isDigit(c1)) {
+
+					move[0] = Character.getNumericValue(c1) - 1;
+					move[1] = ((int) c2) - 65;
+
+				} else {
+
+					move[0] = Character.getNumericValue(c2) - 1;
+					move[1] = ((int) c1) - 65;
+
+				}
+
+			}
+
+			if (play(move, board, yourTurn, prevs)) {
 
 				yourTurn = !yourTurn;
 
@@ -50,96 +92,209 @@ public class Main {
 
 	}
 
-	private static boolean play(String num, int[][] board, boolean yourTurn) {
-
-		num = num.toUpperCase();
+	private static boolean play(int[] move, int[][] board, boolean yourTurn, int[][] prevs) {
 
 		boolean valid = true;
 
-		if (num.length() != 2) {
-			println("O número deve conter dois dígitos!");
+		int x = move[0];
+		int y = move[1];
+
+		int index = yourTurn ? 0 : 1;
+
+		if (board[x][y] != 0) {
+
+			println("Você só pode jogar em slots vazios!");
+
+			valid = false;
+
+		} else if (Arrays.equals(prevs[index], move)) {
+
+			println("Você não pode jogar duas vezes no mesmo lugar");
+
+			valid = false;
+
 		} else {
 
-			//‒
-			//—
+			prevs[index] = move;
 
-			int x, y;
+			for (int i = Math.max(x - 1, 0); i <= Math.min(x + 1, 6); i++) {
+				for (int j = Math.max(y - 1, 0); j <= Math.min(y + 1, 6); j++) {
 
-			char c1 = num.charAt(0);
-			char c2 = num.charAt(1);
-
-			if(Character.isDigit(c1)) {
-
-				y = Character.getNumericValue(c1) - 1;
-				x = ((int) c2) - 65;
-
-			} else {
-
-				x = Character.getNumericValue(c2) - 1;
-				y = ((int) c1) - 65;
-
-			}
-
-			System.out.println(x);
-			System.out.println(y);
-
-			int index = yourTurn ? 0 : 1;
-
-			if (board[x][y] != 0) {
-
-				println("Você só pode jogar em slots vazios!");
-
-				valid = false;
-
-			} else if (prevs[index].equals(num)) {
-
-				println("Você não pode jogar duas vezes no mesmo lugar");
-
-				valid = false;
-
-			} else {
-
-				prevs[index] = num;
-
-				for (int i = Math.max(x - 1, 0); i <= Math.min(x + 1, 6); i++) {
-					for (int j = Math.max(y - 1, 0); j <= Math.min(y + 1, 6); j++) {
-
-						if (board[i][j] == 0) {
-							if (yourTurn) {
-								board[i][j] = 1;
-							} else {
-								board[i][j] = 2;
-							}
+					if (board[i][j] == 0) {
+						if (yourTurn) {
+							board[i][j] = 1;
 						} else {
-							board[i][j] = 0;
+							board[i][j] = 2;
 						}
+					} else {
+						board[i][j] = 0;
 					}
 				}
-
 			}
 		}
+
 
 		return valid;
 
 	}
 
-    /*private static String aiTurn(int[][] board) {
+	private static int[] aiTurn(int[][] board, boolean yourTurn, int difficulty, int[][] prevs) {
 
+		int[] move = new int[2];
 
+		int[][][] analizedBoard = analizeBoard(board, yourTurn);
 
-    }
+		Integer maxLucroValue = Integer.MIN_VALUE;
+		ArrayList<Integer[]> maxLucroIds = new ArrayList<>();
 
-    private static int[] analizePlay(int x, int y) {
+		int maxVantagemValue = Integer.MIN_VALUE;
+		ArrayList<Integer[]> maxVantagemIds = new ArrayList<>();
 
+		int maxBestValue = Integer.MIN_VALUE;
+		ArrayList<Integer[]> maxBestIds = new ArrayList<>();
 
+		for (int i = 0; i < 7; i++) {
+			int[][] line = analizedBoard[i];
 
-    }*/
+			for (int j = 0; j < 7; j++) {
+				int[] column = line[j];
+
+				Integer[] pos = new Integer[2];
+				pos[0] = i;
+				pos[1] = j;
+
+				if (!contains(toPrimitive(pos), prevs)) {
+
+					for (int k = 0; k < 2; k++) {
+
+						int value = column[k];
+
+						ArrayList<Integer[]> maxIds;
+						int maxValue;
+
+						if (k == 0) {
+							maxIds = maxLucroIds;
+							maxValue = maxLucroValue;
+						} else {
+							maxIds = maxVantagemIds;
+							maxValue = maxVantagemValue;
+						}
+
+						if (value > maxValue) {
+
+							if (k == 0) {
+								maxLucroValue = value;
+							} else {
+								maxVantagemValue = value;
+							}
+
+							maxIds.clear();
+							maxIds.add(pos);
+
+						} else if (value == maxValue) {
+
+							maxIds.add(pos);
+
+						}
+					}
+
+					int value = column[0] + column[1] / 3;
+
+					if (value > maxBestValue) {
+
+						maxBestValue = value;
+
+						maxBestIds.clear();
+						maxBestIds.add(pos);
+
+					} else if (value == maxBestValue) {
+
+						maxBestIds.add(pos);
+
+					}
+				}
+			}
+		}
+
+		int[] points = points(board);
+		int turn = yourTurn ? 1 : 0;
+		int win = 25;
+
+		if (difficulty > 0 &&
+				points[turn] + maxLucroValue >= win - 4) {
+
+			move = randomID(maxLucroIds);
+
+		} else if (difficulty > 1 &&
+				points[1 - turn] + 4 >= win) {
+
+			move = randomID(maxVantagemIds);
+
+		} else {
+
+			move = randomID(maxBestIds);
+
+		}
+
+		return move;
+
+	}
+
+	private static int[][][] analizeBoard(int[][] board, boolean yourTurn) {
+
+		int[][][] moves = new int[7][7][4];
+
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < 7; j++) {
+
+				if (board[i][j] == 0) {
+
+					moves[i][j] = analizeMove(i, j, board, yourTurn);
+
+				}
+			}
+		}
+
+		return moves;
+
+	}
+
+	/**
+	 * @return moves
+	 * 0 - lucro
+	 * 1 - vantagem
+	 */
+
+	private static int[] analizeMove(int x, int y, int[][] board, boolean yourTurn) {
+
+		int[] move = new int[2];
+
+		for (int i = Math.max(x - 1, 0); i <= Math.min(x + 1, 6); i++) {
+			for (int j = Math.max(y - 1, 0); j <= Math.min(y + 1, 6); j++) {
+
+				int value = board[i][j];
+
+				int turn = yourTurn ? 1 : 0;
+
+				int index = (int) (Math.signum(value) * turn * Math.abs(value + turn - 2));
+
+				move[index] += turn + value == 2 ? -1 : 1;
+
+			}
+
+		}
+
+		return move;
+
+	}
 
 	private static void sleep(int ms) {
 
-		long time = System.currentTimeMillis();
-
-		while (time + ms > System.currentTimeMillis()) ;
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -147,8 +302,8 @@ public class Main {
 
 		char[] chars = msg.toCharArray();
 
-		for (int i = 0; i < chars.length; i++) {
-			System.out.print(chars[i]);
+		for (char aChar : chars) {
+			System.out.print(aChar);
 			sleep(50);
 		}
 
@@ -203,6 +358,43 @@ public class Main {
 
 		return p1 >= 25 || p2 >= 25;
 
+	}
+
+	private static int[] points(int[][] board) {
+		int[] points = new int[2];
+
+		for (int[] line : board) {
+			for (int column : line) {
+
+				if (column != 0) {
+					points[column - 1]++;
+				}
+
+			}
+		}
+
+		return points;
+
+	}
+
+	private static int[] randomID(ArrayList<Integer[]> arrayList) {
+
+		return toPrimitive(arrayList.get(random.nextInt(arrayList.size())));
+
+	}
+
+	private static int[] toPrimitive(Integer[] array) {
+		return Arrays.stream(array).mapToInt(Integer::intValue).toArray();
+	}
+
+	private static boolean contains(int[] array, int[][] matrix) {
+		boolean contains = false;
+		int i = 0;
+		while (!contains && i < 2) {
+			contains = Arrays.equals(matrix[i], array);
+			i++;
+		}
+		return contains;
 	}
 
 }
