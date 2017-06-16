d4975e95a3c709cd511fadfaa9f6b6d273baa111
Marcelo Vogt <marcelinho.vogt@gmail.com>
2017-06-16 00:56:52 -0300

    AI Completed on Interface Screen 2 Completed Also Screen 2 is the actual game

--- src/com/space/cows/Interface.java
+++ src/com/space/cows/Interface.java
@@ -5,6 +5,7 @@ import br.senai.sc.engine.Game;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.*;
 
 import static java.awt.Color.*;
 
@@ -16,6 +17,7 @@ import static java.awt.Color.*;
  */
 public class Interface extends Game {
 
+	//<editor-fold desc="Contructors">
 	public static void main(String[] args) {
 		Interface interf = new Interface();
 		interf.startGame();
@@ -24,37 +26,51 @@ public class Interface extends Game {
 	public Interface() {
 		super("TSIFOC", 1040, 650);
 		addMouseListener(new MouseInputHandler());
+		addMouseMotionListener(new MouseInputHandler());
 	}
+	//</editor-fold>
 
-
+	//<editor-fold desc="Variable Declarations">
 	private int length = 7;
 	private int slotSize = 84;
 	private int marginTopBottom = 31;
 	private int marginLeftRight = 226;
+	private int boardSize = length * slotSize;
 
 	/**
 	 * Variable that defines in what screen you are in.
 	 * -1 Splash Screen
 	 * 0 Title Screen
 	 * 1 Options Screen
-	 * 2 Game Screen
+	 * 2 Test Screen
 	 * 3 Settings Screen
 	 */
-	private int screen = 0;
+	private int screen = 2;
 
 	private int difficulty = 0;
 
 	private String gamemode = "PC";
 
 	private Color selected = new Color(78, 102, 249);
+	private Random random = new Random();
 
-	// screen 0
+	private int[][] board = new int[length][length];
+	private int[][] prevs;
+	private int farmerTurn = 1;
+
+	//<editor-fold desc="Screen 0">
 	private Image matrisse;
+	//</editor-fold>
 
-	//screen 1
+	//<editor-fold desc="Screen 1">
 	private Image computer;
 	private Image person;
+	//</editor-fold>
 
+	//<editor-fold desc="Screen 2">
+	private Image[] slots;
+	//</editor-fold>
+	//</editor-fold>
 
 	@Override
 	public void init() {
@@ -64,14 +80,24 @@ public class Interface extends Game {
 		computer = carregarImagem("images/computer.png");
 		person = carregarImagem("images/person.png");
 
-	}
+		slots = new Image[3];
+		for (int i = 0; i < slots.length; i++) {
+			slots[i] = carregarImagem("images/slot" + i + ".png");
+		}
 
+		prevs = new int[2][2];
+
+		prevs[0] = new int[]{-1, -1};
+		prevs[1] = new int[]{-1, -1};
+
+	}
 
 
 	@Override
 	public void gameLoop() {
 
 		switch (screen) {
+			//<editor-fold desc="Screen 0">
 			case 0: // tela inicial
 
 				desenharImagem(matrisse, 0, 0);
@@ -81,9 +107,11 @@ public class Interface extends Game {
 				desenharString("COMEÇAR", 430, 520, BLACK, 30);
 
 				break;
+			//</editor-fold>
+			//<editor-fold desc="Screen 1">
 			case 1: // configurações
 
-				desenharRetangulo(0, 0, 1040, 650, new Color(18, 18, 18));
+				drawBG();
 
 				desenharString("Opções:", 463, 60, WHITE, 30);
 				desenharRetangulo(450, 110, 70, 70, new Color(200, 200, 200));
@@ -108,23 +136,29 @@ public class Interface extends Game {
 				desenharString("DIFÍCIL", 470, 312, difficulty == 2 ? selected : Color.BLACK);
 
 				break;
+			//</editor-fold>
+			//<editor-fold desc="Screen 2">
 			case 2:
 
-				desenharRetangulo(0,0,1040,650, BLACK);
+				drawBG();
 
 				for (int i = 0; i < length; i++) {
 					for (int j = 0; j < length; j++) {
 
-						int num = (int) (((double) (i + j)) / 14 * 205 + 50);
+						int num = board[i][j];
 
-						Color color = new Color(num, num, num);
+						Image slot = slots[num];
 
-						desenharRetangulo(marginLeftRight + slotSize * i, marginTopBottom + slotSize * j, slotSize, slotSize, color);
+						int x1 = marginLeftRight + slotSize * i;
+						int y1 = marginTopBottom + slotSize * j;
+
+						desenharImagem(slot, x1, y1);
 
 					}
 				}
 
 				break;
+			//</editor-fold>
 		}
 
 	}
@@ -133,6 +167,271 @@ public class Interface extends Game {
 	public void aposTermino() {
 
 	}
+
+	private void makeMove(int x, int y) {
+
+		int[] pos = new int[]{x, y};
+
+		if (!Arrays.equals(prevs[farmerTurn], pos)) {
+
+			if (board[x][y] == 0) {
+
+				for (int i = max(x - 1, 0); i <= min(x + 1, length - 1); i++) {
+					for (int j = max(y - 1, 0); j <= min(y + 1, length - 1); j++) {
+
+						int slot = board[i][j];
+
+						board[i][j] = (slot == 0 ? 1 : 0) * (2 - farmerTurn);
+
+					}
+				}
+
+				prevs[farmerTurn] = pos;
+
+				farmerTurn = 1 - farmerTurn;
+
+				if(gamemode.charAt(1 - farmerTurn) == 'C') {
+
+					aiPlay();
+
+				}
+
+			} else {
+				// Alert player to play only on free slots
+			}
+		} else {
+			// Alert player not to play twice on the same place
+		}
+	}
+
+	//<editor-fold desc="Artificial Intelligence">
+	private void aiPlay() {
+
+		int delay = random.nextInt(2000) + 500;
+
+		new Timer().schedule(
+				new TimerTask() {
+					@Override
+					public void run() {
+
+						int[] move = aiTurn();
+
+						makeMove(move[0], move[1]);
+
+					}
+				}, delay
+		);
+
+	}
+
+	private int[] aiTurn() {
+
+		int[] move;
+
+		int[][][] analizedBoard = analizeBoard(board);
+
+		Integer maxProfitValue = Integer.MIN_VALUE;
+		ArrayList<Integer[]> maxProfitIds = new ArrayList<>();
+
+		int maxAdvantageValue = Integer.MIN_VALUE;
+		ArrayList<Integer[]> maxAdvantageIds = new ArrayList<>();
+
+		int maxBestValue = Integer.MIN_VALUE;
+		ArrayList<Integer[]> maxBestIds = new ArrayList<>();
+
+		for (int i = 0; i < length; i++) {
+			int[][] line = analizedBoard[i];
+
+			for (int j = 0; j < length; j++) {
+				int[] column = line[j];
+
+				Integer[] pos = new Integer[2];
+				pos[0] = i;
+				pos[1] = j;
+
+				if (!contains(toPrimitive(pos))) {
+
+					for (int k = 0; k < 2; k++) {
+
+						int value = column[k];
+
+						ArrayList<Integer[]> maxIds;
+						int maxValue;
+
+						if (k == 0) {
+							maxIds = maxProfitIds;
+							maxValue = maxProfitValue;
+						} else {
+							maxIds = maxAdvantageIds;
+							maxValue = maxAdvantageValue;
+						}
+
+						if (value > maxValue) {
+
+							if (k == 0) {
+								maxProfitValue = value;
+							} else {
+								maxAdvantageValue = value;
+							}
+
+							maxIds.clear();
+							maxIds.add(pos);
+
+						} else if (value == maxValue) {
+
+							maxIds.add(pos);
+
+						}
+					}
+
+					int value = column[0] + column[1] / (3 - difficulty);
+
+					if (value > maxBestValue) {
+
+						maxBestValue = value;
+
+						maxBestIds.clear();
+						maxBestIds.add(pos);
+
+					} else if (value == maxBestValue) {
+
+						maxBestIds.add(pos);
+
+					}
+				}
+			}
+		}
+
+		int[] points = points();
+		int turn = farmerTurn;
+		int win = (int) Math.ceil(length * length / 2);
+
+		if (difficulty > 0 &&
+				points[turn] + maxProfitValue >= win) {
+
+			move = randomID(maxProfitIds);
+
+		} else if (difficulty > 1 &&
+				points[1 - turn] + Math.ceil(length / 3) - 1 >= win) {
+
+			maxProfitIds.clear();
+			maxProfitValue = Integer.MIN_VALUE;
+
+			for (Integer[] advantage : maxAdvantageIds) {
+
+				int x = advantage[0];
+				int y = advantage[1];
+
+				int profit = analizedBoard[x][y][0];
+
+				if(profit > maxProfitValue) {
+
+					maxProfitValue = profit;
+
+					maxProfitIds.clear();
+					maxProfitIds.add(advantage);
+
+				} else if(profit == maxProfitValue) {
+
+					maxProfitIds.add(advantage);
+
+				}
+
+			}
+
+			move = randomID(maxProfitIds);
+
+		} else {
+
+			move = randomID(maxBestIds);
+
+		}
+
+		return move;
+
+	}
+
+	private int[][][] analizeBoard(int[][] board) {
+
+		int[][][] moves = new int[length][length][2];
+
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < length; j++) {
+
+				if (board[i][j] == 0) {
+
+					moves[i][j] = analizeMove(i, j, board);
+
+				}
+			}
+		}
+
+		return moves;
+
+	}
+
+	/**
+	 * @return moves
+	 * 0 - profit
+	 * 1 - advantage
+	 */
+
+	private int[] analizeMove(int x, int y, int[][] board) {
+
+		int[] move = new int[2];
+
+		for (int i = Math.max(x - 1, 0); i <= Math.min(x + 1, length - 1); i++) {
+			for (int j = Math.max(y - 1, 0); j <= Math.min(y + 1, length - 1); j++) {
+
+				int value = board[i][j];
+
+				int turn = farmerTurn;
+
+				int index = (int) (Math.signum(value) * turn * Math.abs(value + turn - 2));
+
+				move[index] += turn + value == 2 ? -1 : 1;
+
+			}
+
+		}
+
+		return move;
+
+	}
+
+	private int[] points() {
+		int[] points = new int[2];
+
+		for (int[] line : board) {
+			for (int column : line) {
+
+				if (column != 0) {
+					points[column - 1]++;
+				}
+
+			}
+		}
+
+		return points;
+
+	}
+
+	private int[] randomID(ArrayList<Integer[]> arrayList) {
+
+		return toPrimitive(arrayList.get(random.nextInt(arrayList.size())));
+
+	}
+
+	private int[] toPrimitive(Integer[] array) {
+		return Arrays.stream(array).mapToInt(Integer::intValue).toArray();
+	}
+
+	private boolean contains(int[] array) {
+		
+		return Arrays.equals(array, prevs[farmerTurn]);
+		
+	}
+	//</editor-fold>
 
 	private class MouseInputHandler extends MouseAdapter {
 
@@ -147,15 +446,16 @@ public class Interface extends Game {
 			System.out.println("Y " + y);
 
 			switch (screen) {
+				//<editor-fold desc="Screen 0">
 				case 0:
 
 					if (between(415, x, 605) && between(490, y, 530)) {
-
 						screen = 1;
-
 					}
 
 					break;
+				//</editor-fold>
+				//<editor-fold desc="Screen 1">
 				case 1:
 					if (between(450, x, 590)) {
 
@@ -175,12 +475,45 @@ public class Interface extends Game {
 						}
 					}
 					break;
+				//</editor-fold>
+				//<editor-fold desc="Screen 2">
+				case 2:
+					if (gamemode.charAt(1 - farmerTurn) == 'P' && between(marginLeftRight, x, marginLeftRight + boardSize) && between(marginTopBottom, y, marginTopBottom + boardSize)) {
+
+						int x1 = x - marginLeftRight;
+						int y1 = y - marginTopBottom;
+
+						int x2 = (int) Math.floor((double) x1 / slotSize);
+						int y2 = (int) Math.floor(y1 / slotSize);
+
+						makeMove(x2, y2);
+
+					}
+					break;
+				//</editor-fold>
 			}
 
 		}
 	}
 
+	//<editor-fold desc="Extra Functions">
+	private int min(int n, int m) {
+		return Math.min(n, m);
+	}
+
+	private int max(int n, int m) {
+		return Math.max(n, m);
+	}
+
+	private void drawBG() {
+
+		desenharRetangulo(0, 0, 1040, 650, new Color(18, 18, 18));
+
+	}
+
 	private boolean between(int min, int num, int max) {
 		return min < num && num < max;
 	}
+	//</editor-fold>
+
 }
---
+++ src/com/space/cows/Test.java
@@ -0,0 +1,234 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.space.cows;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.LayoutManager;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferStrategy;
+import java.awt.image.ImageObserver;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import br.senai.sc.engine.*;
+
+public abstract class Test extends Canvas {
+	private static final long serialVersionUID = 6058040659371962305L;
+	private JFrame container;
+	private BufferStrategy strategy;
+	private boolean gameRunning = true;
+	private boolean sairAoTermino = false;
+	private Graphics2D graphics2D;
+	private Fps fps;
+	private Map<String, Mp3> musicas;
+	private Map<String, CustomFont> customFonts;
+
+	public Test() {
+		Dimension fullscreen = Toolkit.getDefaultToolkit().getScreenSize();
+		Utils.getInstance().setHeight(fullscreen.height);
+		Utils.getInstance().setWidth(fullscreen.width);
+		this.container = new JFrame(Utils.getInstance().getNomeJogo());
+		this.container.setUndecorated(true);
+		JPanel panel = (JPanel)this.container.getContentPane();
+		panel.setPreferredSize(new Dimension(Utils.getInstance().getWidth(), Utils.getInstance().getHeight()));
+		panel.setLayout((LayoutManager)null);
+		this.setBounds(0, 0, Utils.getInstance().getWidth(), Utils.getInstance().getHeight());
+		panel.add(this);
+		this.setIgnoreRepaint(true);
+		this.container.pack();
+		this.container.setResizable(false);
+		this.container.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		this.fps = new Fps();
+		this.musicas = new HashMap();
+		this.customFonts = new HashMap();
+		this.init();
+		this.container.setOpacity(0.55f);
+		this.container.setVisible(true);
+		this.requestFocus();
+		this.createBufferStrategy(2);
+		this.strategy = this.getBufferStrategy();
+	}
+
+	public Test(String nomeJogo, int width, int height) {
+		Utils.getInstance().setNomeJogo(nomeJogo);
+		Utils.getInstance().setHeight(height);
+		Utils.getInstance().setWidth(width);
+		this.container = new JFrame(Utils.getInstance().getNomeJogo());
+		JPanel panel = (JPanel)this.container.getContentPane();
+		panel.setPreferredSize(new Dimension(Utils.getInstance().getWidth() - 10, Utils.getInstance().getHeight() - 10));
+		panel.setLayout((LayoutManager)null);
+		this.setBounds(0, 0, Utils.getInstance().getWidth(), Utils.getInstance().getHeight());
+		panel.add(this);
+		this.setIgnoreRepaint(true);
+		this.container.pack();
+		this.container.setResizable(false);
+		this.container.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		this.fps = new Fps();
+		this.musicas = new HashMap();
+		this.customFonts = new HashMap();
+		this.init();
+		this.container.setLocationRelativeTo((Component)null);
+		this.container.setVisible(true);
+		this.requestFocus();
+		this.createBufferStrategy(2);
+		this.strategy = this.getBufferStrategy();
+	}
+
+	public abstract void init();
+
+	public void startGame() {
+		while(this.gameRunning) {
+			this.graphics2D = (Graphics2D)this.strategy.getDrawGraphics();
+			this.fps.updateFPS();
+			this.graphics2D.setColor(Color.white);
+			this.graphics2D.fillRect(0, 0, Utils.getInstance().getWidth(), Utils.getInstance().getHeight());
+			this.gameLoop();
+			this.graphics2D.dispose();
+			this.strategy.show();
+			this.fps.synchronize();
+		}
+
+		this.aposTermino();
+		if(this.sairAoTermino) {
+			System.exit(0);
+		}
+
+	}
+
+	public void desenharGif(Image image, int x, int y) {
+		this.graphics2D.drawImage(image, x, y, this.container);
+	}
+
+	public void desenharImagem(Image image, int x, int y) {
+		this.graphics2D.drawImage(image, x, y, (ImageObserver)null);
+	}
+
+	public void desenharString(String mensagem, int x, int y) {
+		this.graphics2D.drawString(mensagem, x, y);
+	}
+
+	public void desenharString(String mensagem, int x, int y, Color color) {
+		this.graphics2D.setColor(color);
+		this.graphics2D.drawString(mensagem, x, y);
+	}
+
+	public void desenharString(String mensagem, int x, int y, Color color, int fontSize) {
+		this.graphics2D.setColor(color);
+		this.graphics2D.setFont(new Font("Arial", 1, fontSize));
+		this.graphics2D.drawString(mensagem, x, y);
+	}
+
+	public void desenharString(String mensagem, int x, int y, Color color, int fontSize, String fontName) {
+		this.graphics2D.setColor(color);
+		this.graphics2D.setFont(new Font(fontName, 1, fontSize));
+		this.graphics2D.drawString(mensagem, x, y);
+	}
+
+	public void desenharString(String mensagem, int x, int y, Color color, int fontSize, String fontName, int fontStyle) {
+		this.graphics2D.setColor(color);
+		this.graphics2D.setFont(new Font(fontName, fontStyle, fontSize));
+		this.graphics2D.drawString(mensagem, x, y);
+	}
+
+	public Image carregarImagem(String path) {
+		return Utils.getInstance().loadImage(path);
+	}
+
+	public void finalizarJogo() {
+		this.gameRunning = false;
+	}
+
+	public abstract void aposTermino();
+
+	public abstract void gameLoop();
+
+	public void sairAoTerminar() {
+		this.sairAoTermino = true;
+	}
+
+	public void alterarFramesPorSegundos(int fps) {
+		this.fps = new Fps(fps);
+	}
+
+	public void adicionarAudio(String nome, String path) {
+		Mp3 mp3 = new Mp3();
+		mp3.carregar(path);
+		if(this.musicas.get(nome) != null) {
+			this.musicas.remove(nome);
+		}
+
+		this.musicas.put(nome, mp3);
+	}
+
+	public void tocarAudio(String nome) {
+		String audioName = ((Mp3)this.musicas.get(nome)).getAudioName();
+		this.musicas.remove(nome);
+		Mp3 mp3 = new Mp3();
+		mp3.carregar(audioName);
+		this.musicas.put(nome, mp3);
+		((Mp3)this.musicas.get(nome)).iniciar();
+	}
+
+	public void pararAudio(String nome) {
+		((Mp3)this.musicas.get(nome)).finalizar();
+	}
+
+	public boolean audioIsCompleted(String nome) {
+		return this.musicas.get(nome) != null?((Mp3)this.musicas.get(nome)).isCompleted():true;
+	}
+
+	public void removerAudio(String nome) {
+		this.musicas.remove(nome);
+	}
+
+	public void desenharRetangulo(int x, int y, int width, int height, Color color) {
+		this.graphics2D.setColor(color);
+		this.graphics2D.fillRect(x, y, width, height);
+	}
+
+	public void desenharCirculo(int x, int y, int width, int height, Color color) {
+		this.graphics2D.setColor(color);
+		this.graphics2D.fillOval(x, y, width, height);
+	}
+
+	public void addNewFont(String name, String path, float size, int style) {
+		CustomFont cf = new CustomFont(path, size, style);
+		this.customFonts.put(name, cf);
+	}
+
+	public void setFont(String name) {
+		this.graphics2D.setFont(((CustomFont)this.customFonts.get(name)).getCustomFont());
+	}
+
+	public Graphics2D getGraphics2D() {
+		return this.graphics2D;
+	}
+
+	public int getWidth() {
+		return Utils.getInstance().getWidth();
+	}
+
+	public int getHeight() {
+		return Utils.getInstance().getHeight();
+	}
+}
